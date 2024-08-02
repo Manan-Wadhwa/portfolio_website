ed870bfb4c2463695b0640caba2ae45eb0450aa5
Manan Wadhwa <wadhwamanan11@outlook.com>
2024-08-02 06:55:41 +0530

    collision mapping done

--- index.js
+++ index.js
@@ -6,33 +6,59 @@ const c = canvas.getContext('2d')
 canvas.width = 1024; // width of the screen in pixels
 canvas.height = 576; // height of the screen in pixels
 
+const scaledCanvas  = {
+    width: canvas.width/4,
+    height: canvas.height/4
+}
 const gravity = 0.5;
 
-class Player {
-    constructor(position){
-        this.position=position
-        this.velocity= {
-            x: 0 ,y: 1
-        }
-        this.height = 100
-    } 
-    draw(){
-        c.fillStyle = "red"
-        c.fillRect(this.position.x, this.position.y, 100, this.height)
-    } 
-    update(){
-        this.draw()
-        this.position.y += this.velocity.y
-        this.position.x += this.velocity.x
-        
-        if(this.position.y + this.height + this.velocity.y < canvas.height)
-            this.velocity.y += gravity
-        else this.velocity.y = 0
-    }
+const tileCollisions2d = []
+for (let i=0; i<tileCollisions.length; i+=36){
+    tileCollisions2d.push(tileCollisions.slice(i,i+36))
+}
+const floorCollisions2d = []
+for (let i=0; i<floorCollisions.length; i+=36){
+    floorCollisions2d.push(floorCollisions.slice(i,i+36))
 }
 
+
+const collisoionBlocks = []
+tileCollisions2d.forEach((row,y) => {
+    row.forEach((symbol,x) => {
+      if(symbol === 202){
+        console.log('draw a block here')
+        collisoionBlocks.push(new CollisoionBlock({
+            position: {
+                x:x*16, 
+                y: y*16}
+        }))
+
+      }  
+    })
+
+})
+
+floorCollisions2d.forEach((row,y) => {
+    row.forEach((symbol,x) => {
+      if(symbol === 202){
+        console.log('draw a block here')
+        collisoionBlocks.push(new CollisoionBlock({
+            position: {
+                x:x*16, 
+                y: y*16}
+        }))
+
+      }  
+    })
+
+})
+
+
+
+
+
+// player implementation
 const player = new Player({x: 0, y: 0})
-const player2 = new Player({x: 200, y: 100})
 
 const keys = {
     d:{pressed: false},
@@ -40,14 +66,36 @@ const keys = {
 
 }
 
+// sprites implementation
+const background = new Sprite({position:{
+    x:0,
+    y:0
+},
+imageSrc: 'images/background.png'
+})
+
+
+
+
 
 function animate(){
     window.requestAnimationFrame(animate)
     
     c.fillStyle = "white"
     c.fillRect(0, 0, canvas.width, canvas.height)
+
+    c.save()
+    c.scale(4,4)
+    c.translate(0,-background.image.height+scaledCanvas.height)
+    background.update()
+    collisoionBlocks.forEach((CollisoionBlock)=>{
+        CollisoionBlock.update()
+    })
+    c.restore()
+
+    
+
     player.update()
-    player2.update()
 
     player.velocity.x =  0
     if (keys.d.pressed) player.velocity.x=5
@@ -57,7 +105,6 @@ function animate(){
 }
 
 animate()
-
 window.addEventListener('keydown', (event) => {
     switch(event.key){
         case 'd':
